6985027b675ed7967118526e2bae4134a684ca9c
2026-Nathan <nnelsen@students.unwsp.edu>
2026-01-30 14:24:25 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,7 +1,8 @@
 def personal_information():
-  print('Cranjis McBasketball')
-
-
+  print('My name is Cranjis McBasketball')
+  print('My address is 3345 Oak Road, Jamestown, RI, 92489')
+  print('My phone number is 133-746-8992')
+  print('Major in Communications')
 
 # Line which calls the above function.
 personal_information()
