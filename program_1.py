18a1e4fa37000d87de12e8edcd225faae1aa2fa4
2026-Nathan <nnelsen@students.unwsp.edu>
2026-01-30 14:22:48 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -3,7 +3,5 @@ def personal_information():
   print('3345 Oak Road, Jamestown, RI, 92489')
   print('133-746-8992')
   print('Major in Communications')
-
-
 # Line which calls the above function.
 personal_information()
