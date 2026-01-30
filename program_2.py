a956966cf94fb084bf8444cb18347064e5805807
2026-Nathan <nnelsen@students.unwsp.edu>
2026-01-30 14:50:11 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,13 +1,21 @@
+# By Nathan Nelsen
+# Written 1/30/26
+# Average Age
 
 def average_age():
-    # Get User Input
+
+    a = 25
+    b = 23
+    c = 32
+    d = 21
+    e = 26
 
 
     # Sum ages
-
+    result = a + b + c + d + e
     # Average the ages
-
+    result = result / 5
     # Print the results
-
+    print(result)
 # Line which calls the above function.
 average_age()
