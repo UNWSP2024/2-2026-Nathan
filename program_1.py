b6115f5b2c1602ef2a7849ce9f2df9cdd5a7acc8
2026-Nathan <nnelsen@students.unwsp.edu>
2026-01-30 14:58:18 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,21 +1,16 @@
 # By Nathan Nelsen
 # Written 1/30/26
-# Average Age
-
-def average_age():
-
-    a = 25
-    b = 23
-    c = 32
-    d = 21
-    e = 26
+# Personal Information
 
 
-    # Sum ages
-    result = a + b + c + d + e
-    # Average the ages
-    result = result / 5
-    # Print the results
-    print(result)
-# Line which calls the above function.
-average_age()
+def personal_information():
+    name = "Cranjis McBasketball"
+    address = "3345 Oak Road, Jamestown, RI, 92489"
+    phone_number = "133-746-8992"
+    major = "Communications"
+    print("Name:",name)
+    print("Address:",address)
+    print("Phone Number",phone_number)
+    print("Major:",major)
+#Line which calls the above function.
+personal_information()
